f297f3763e8d7227eae7f84ee522eb724c9c8d2e
DottoGC <otttoguitar@gmail.com>
2020-06-10 08:36:05 +0000

    actualizacion/finalizacion modulo de memoria RAM

---
+++ modulo1/ram/.memo_201020975_201212623.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/jiren_pruebas/proyecto1/SO1P1/modulo1/ram/memo_201020975_201212623.mod := { echo  /home/jiren_pruebas/proyecto1/SO1P1/modulo1/ram/memo_201020975_201212623.o; echo; } > /home/jiren_pruebas/proyecto1/SO1P1/modulo1/ram/memo_201020975_201212623.mod
