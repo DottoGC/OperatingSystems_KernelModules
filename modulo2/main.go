5ee1df7859b12e61d1ff9220a9408f58d89cdda0
keniacode <kenia.apk@gmail.com>
2020-06-12 15:54:33 -0600

    Frontend implementado

--- modulo2/main.go
+++ modulo2/main.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+	"time"
+)
 
-func main() {
-    fmt.Println("hello world")
+//Create a struct that holds information to be displayed in our HTML file
+type Welcome struct {
+	Name string
+	Time string
 }
 
+func main() {
+
+	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+
+	templates := template.Must(template.ParseFiles("static/index.html"))
+	http.Handle("/static/", http.FileServer(http.Dir(".")))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		if name := r.FormValue("name"); name != "" {
+			welcome.Name = name
+		}
+
+		if err := templates.ExecuteTemplate(w, "index.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
+	//Print any errors from starting the webserver using fmt
+
+	http.ListenAndServe(":8080", nil)
+}
